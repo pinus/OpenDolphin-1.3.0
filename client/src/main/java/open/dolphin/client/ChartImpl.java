c7dc29802c10dca16352992777507b01c1beaf9b
pinus <nba01176@nifty.ne.jp>
2025-12-18 15:56:03 +0900

    Prevent chart frame flickering with stage manager by invoking `toFront` on parent frame

--- client/src/main/java/open/dolphin/client/ChartImpl.java
+++ client/src/main/java/open/dolphin/client/ChartImpl.java
@@ -1454,6 +1454,9 @@ public class ChartImpl extends AbstractMainTool implements Chart, IInfoModel, Wi
 
     @Override
     public void stop() {
+        // ステージマネージャ使用時に, chart frame がパタつくのを防ぐ
+        getContext().getFrame().toFront();
+
         if (providers != null) {
             for (String s : providers.keySet()) {
                 ChartDocument doc = providers.get(s);
